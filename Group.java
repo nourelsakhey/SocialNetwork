849c1d75764fa87117e5f8c30d7010d127467a4b
Fatima <44647655+fatima196@users.noreply.github.com>
2019-12-24 16:27:17 +0200

    Update Group.java

--- Group.java
+++ Group.java
@@ -11,17 +11,51 @@ package socialnetwork;
  */
 public class Group {
     
+String name;
 User owner;
-String content;
-String privacy;
-int likes_no;
-int share_no;
-
-controller groupController;
-+isHash:boolean
+User [] joined;
+Post [] posts; 
+boolean isprivate;
 
 
 
+ControllerAll groupController;
 
+
+void Group(boolean type){
+    
+}
+
+void create(User admin){
+    
+}
+
+void joinGroup(String joined){
+
+}
+
+void remove(User person){
+
+}
+    
+void change_privacy (String privacy){
+    
+}
+void delete(User admin){
+
+}
+
+boolean checkPrivacy(){
+    return true;
+}
+
+boolean accept(User person){
+    return true;
+} 
+
+void addPost(Post p){
+     
+ }
+//github
 
 }
