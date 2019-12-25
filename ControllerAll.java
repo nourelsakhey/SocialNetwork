6b88457e3d9a409b6be2ca9a56942c70de75daef
mai41 <36419468+mai41@users.noreply.github.com>
2019-12-25 17:28:05 +0200

    Update ControllerAll.java

--- ControllerAll.java
+++ ControllerAll.java
@@ -1,8 +1,17 @@
+package socialnetwork;
+import java.util.ArrayList;
+import java.util.Scanner;
+import socialnetwork.Group;
+import socialnetwork.Page;
+import socialnetwork.Post;
+import socialnetwork.User;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package socialnetwork;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -12,24 +21,32 @@ import java.util.Scanner;
  * @author lenovo
  */
 public class ControllerAll {
-      ArrayList<User> userList=new ArrayList<>();
+
+   static ArrayList<User> userList=new ArrayList<>();
     
-    ArrayList<Group> groupList=new ArrayList<>();
+   static ArrayList<Group> groupList=new ArrayList<>();
     
-    ArrayList<Post> postList=new ArrayList<>();
+   static ArrayList<Post> postList=new ArrayList<>();
     
-    ArrayList<Page> pageList=new ArrayList<>();
-    public static void main(String[] args) {
-        // TODO code application logic here
-        User u=new User();
-        System.out.println("Enter 1 to create accout");
-        System.out.println("Enter 2 to login");
-        System.out.println("Enter 3 to Add friend");
-        System.out.println("Enter 4 to Accept friend");
-        Scanner sc=new Scanner(System.in);
-        int c=sc.nextInt();
-        switch (c){
-            case 1:
+   static ArrayList<Page> pageList=new ArrayList<>();
+     public static void main(String[] args) {
+         
+         User u=new User ();
+         System.out.println("Enter 1 to create account  ");
+         System.out.println("Enter 2 to login ");
+         Scanner sc=new Scanner(System.in);
+         int c=sc.nextInt();
+         
+         switch (c) {
+             
+             case 1:
+                 System.out.println("Enter name , gender ,mail , password");
+                 String name=sc.nextLine();
+                 String gender=sc.nextLine();
+                 String mail=sc.nextLine();
+                 String pass=sc.nextLine();
+               userList.add(u.controller.create_account(name, gender, mail, pass));
+
             case 2:
                 System.out.println("Enter name");
                 String addname=sc.nextLine();
@@ -39,6 +56,6 @@ public class ControllerAll {
                 System.out.println("Enter name");
                 String acceptname=sc.nextLine();
                 u.accept_friend(acceptname);
-        }
+         }
     }
 }
