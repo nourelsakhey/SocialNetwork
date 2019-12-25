7d233a4bcc4c2c268e133e2a1003be05f3ea7217
mai41 <36419468+mai41@users.noreply.github.com>
2019-12-25 17:16:41 +0200

    Update SocialNetwork.java

--- SocialNetwork.java
+++ SocialNetwork.java
@@ -14,9 +14,6 @@ public class SocialNetwork {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
     
 }
